5f00d5fbe93ab4e4a8309c917dfcd7e00118d435
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2023-01-16 23:28:42 +0400

    pass through progress

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -25,7 +25,7 @@ const ListItem = ({ task, getData }) => {
       <div className="info-container">
         <TickIcon/>
         <p className="task-title">{task.title}</p>
-        <ProgressBar/>
+        <ProgressBar progress={task.progress}/>
       </div>
 
       <div className="button-container">
