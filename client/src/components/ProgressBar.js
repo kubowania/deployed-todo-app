048528f47c5c2962bb44896cc58c9cae8dfd8a12
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2023-01-16 23:28:09 +0400

    add progress bar

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,6 +1,19 @@
-const ProgressBar = () => {
+const ProgressBar = ({ progress }) => {
+  const colors = [
+    'rgb(255, 214, 161)',
+    'rgb(255, 175, 163)',
+    'rgb(108, 115, 148)',
+    'rgb(141, 181, 145)'
+  ]
+
+  const randomColor = colors[Math.floor(Math.random() * colors.length)]
+
   return (
-    <div>
+    <div className="outer-bar">
+      <div
+        className="inner-bar"
+        style={{ width: `${progress}%`, backgroundColor: randomColor }}
+      ></div>
     </div>
   )
 }
