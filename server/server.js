179104638cd19ac2691ff26d119341dbc63f3945
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2023-03-17 16:55:41 +0200

    pass through 'err' not 'error'

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/todos/:userEmail', async (req, res) => {
     const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
     res.json(todos.rows)
   } catch (err) {
-    console.error(error)
+    console.error(err)
   }
 })
 
